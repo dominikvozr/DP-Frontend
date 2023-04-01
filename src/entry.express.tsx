0e732197af6c8446e6f6a7f51f401c8f602a0a7d
Dominik Vozár <dominikvozr@gmail.com>
2023-04-01 23:53:03 +0200

    refactor: upgrade qwik and adjust code

--- src/entry.express.tsx
+++ src/entry.express.tsx
@@ -22,7 +22,7 @@ declare global {
 import compression from 'compression';
 
 // Directories where the static assets are located
-const distDir = join(fileURLToPath(import.meta.url), '..', '..', 'dist');
+const distDir = join(fileURLToPath(import.meta.url), 'app', '..', '..', 'dist');
 const buildDir = join(distDir, 'build');
 
 // Allow for dynamic port
@@ -41,13 +41,13 @@ app.use(compression());
 // Static asset handlers
 // https://expressjs.com/en/starter/static-files.html
 app.use(`/app/build`, express.static(buildDir, { immutable: true, maxAge: '1y' }));
-app.use(express.static(distDir, { redirect: false }));
+app.use('/app', express.static(distDir, { redirect: false }));
 
 // Use Qwik City's page and endpoint request handler
-app.use(router);
+app.use('/app', router);
 
 // Use Qwik City's 404 handler
-app.use(notFound);
+app.use('/app', notFound);
 
 // Start the express server
 app.listen(PORT, () => {
